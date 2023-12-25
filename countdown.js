7c5a82d237de54e22637547d7c8235ba9687198b
Mithilesh Reddy <mithileshreddy2@gmail.com>
2023-12-25 09:18:40 +0530

    ist-updated

--- countdown.js
+++ countdown.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const newYearDate = new Date('January 1, 2024 00:00:00').getTime();
+    const newYearDate = new Date('January 1, 2024 00:00:00 GMT+0530').getTime();
 
     function updateCountdown() {
-        const currentDate = new Date().getTime();
+        const currentDate = new Date().getTime() + (5.5 * 60 * 60 * 1000); // Add 5.5 hours for IST
         const difference = newYearDate - currentDate;
 
         const days = Math.floor(difference / (1000 * 60 * 60 * 24));
